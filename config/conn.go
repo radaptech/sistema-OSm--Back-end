331e0856508278aa962752bf3ddf3f56929ca53a
davi <davifernadesr@gmail.com>
2026-08-12 14:57:59 -0300

    feat: adicionar configuração de conexão com PostgreSQL e migrações de banco de dados

---
+++ config/conn.go
@@ -0,0 +1,87 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
+)
+
+type Conexao interface {
+	Conn(configEnv *VariaveisDeAmbiente) (*pgxpool.Pool, error)
+}
+
+type ConnPostgresql struct {
+	Pool *pgxpool.Pool
+}
+
+func (p *ConnPostgresql) Conn(configEnv *VariaveisDeAmbiente) (*pgxpool.Pool, error) {
+
+	// Formato: postgres://usuario:senha@host:porta/database
+	connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		configEnv.DB_SERVER,
+		configEnv.DB_PORT,
+		configEnv.DB_USER,
+		configEnv.DB_PASSWORD,
+		configEnv.DATABASE,
+		configEnv.DB_SSLMODE,
+	)
+
+	config, err := pgxpool.ParseConfig(connString)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao configurar Pool do banco de dados: %v", err)
+	}
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao se conectar com o banco de dados, %v", err)
+	}
+
+	err = pool.Ping(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("erro ao verificar se o banco de dados esta ativo", err)
+	}
+
+	s := pool.Stat()
+	log.Printf("conn totais: %d | em uso: %d | ociosas: %d\n", s.TotalConns(), s.AcquiredConns(), s.IdleConns())
+
+	return pool, nil
+}
+
+func (p *ConnPostgresql) RunMigrationPostgress(db *pgxpool.Pool) error {
+
+	sqlDB := stdlib.OpenDBFromPool(db)
+	driver, err := pgx.WithInstance(sqlDB, &pgx.Config{})
+	if err != nil {
+
+		return fmt.Errorf("erro ao iniciar drive da migração, %w", err)
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://database/migrate",
+		"postgres", driver)
+	if err != nil {
+		return fmt.Errorf("erro ao instanciar migraççao no banco de dados")
+	}
+	dir, _ := os.Getwd()
+	fmt.Println("O programa está rodando na pasta:", dir)
+	fmt.Println("Tentando ler migrações de:", dir+"/database/migrate")
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("erro ao aplicar migrações: %w", err)
+	}
+
+	if err == migrate.ErrNoChange {
+		log.Println("Nenhuma migração nova para aplicar.")
+	} else {
+		log.Println("Migrações aplicadas com sucesso!")
+	}
+
+	log.Println("Migrações aplicadas no banco de dados!....")
+	return nil
+}
